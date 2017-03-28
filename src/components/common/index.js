6a303619cbc328873e6516a242196ba996e436d6
Edward Park <ed.sh.park@gmail.com>
2017-03-27 21:16:37 -0700

    creating index lookup for common components

---
+++ src/components/common/index.js
@@ -0,0 +1,11 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as CardSection } from './CardSection';
+export { default as Header } from './Header';
+
+// If you use this syntax, you need to export components
+// As objects, not default
+// export * from './Button';
+// export * from './Card';
+// export * from './CardSection';
+// export * from './Header';
