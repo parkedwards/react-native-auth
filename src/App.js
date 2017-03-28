8b298a71d47a55b2b82e66acd520a1809d1cac20
Edward Park <ed.sh.park@gmail.com>
2017-03-27 23:58:38 -0700

    created local state to trigger conditional rendering depending on loggedIn state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: null
+    };
+  }
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBPUn_lmfVvJy8BV5zVHhnpR01JD-XKcrI',
+      authDomain: 'react-native-auth-17952.firebaseapp.com',
+      databaseURL: 'https://react-native-auth-17952.firebaseio.com',
+      storageBucket: 'react-native-auth-17952.appspot.com',
+      messagingSenderId: '1025023417472'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderLogin() {
+    console.log(this.state);
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+              </Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>The App View</Text>
+        <Header type={'Authentication'} />
+        {this.renderLogin()}
       </View>
     );
   }
